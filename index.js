729382014dc4cb3a52236299f80edcb891392b52
Christian <christianbarahona2450@gmail.com>
2020-07-11 04:27:12 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+require('./user.model');
+require('./config/passportConfig');
+const app = express()
+
+const uri = "mongodb+srv://Hona27:2450chris@cluster0.iy7pc.mongodb.net/Cluster0?retryWrites=true&w=majority" 
+
+const rtsIndex = require('./routes/index.router');
+
+const PORT = 3000
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(passport.initialize());
+app.use('/api', rtsIndex);
+
+
+
+mongoose.connect(uri, { useNewUrlParser: true },(err,client) =>{
+  if (err) throw err;
+  console.log('Successfully connected to DB server');
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+      var valErrors = [];
+      Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+      res.status(422).send(valErrors)
+  }
+});
+
+//start server
+app.listen(PORT, () => {
+  console.log(`app is listening to PORT ${PORT}`)
+})
+
+
